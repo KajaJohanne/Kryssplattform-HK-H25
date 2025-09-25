c588c194995d2a99ebe27227f29ee33945127fe6
Kaja Johanne Myrmel <145992776+KajaJohanne@users.noreply.github.com>
2025-09-25 11:33:54 +0200

    endringer

--- types/post.ts
+++ types/post.ts
@@ -1,6 +1,12 @@
-
 export interface PostData {
-    id: string;
-    title: string;
-    description: string;
+  id: string;
+  title: string;
+  description: string;
+  imageUri: string;
+  comments: PostComment[];
+}
+
+export interface PostComment {
+  author: string;
+  comment: string;
 }
